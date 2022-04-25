da98baffcaf17580b0c93a4ff8709d18bebaeb35
Semini J.P.D.L <dulmilaknasewmz9811@gmail.com>
2022-04-25 08:39:54 +0530

    Employee  Service

--- src/com/EmployeeService.java
+++ src/com/EmployeeService.java
@@ -1,5 +1,83 @@
-package com;
+package com; 
+import model.Employee; 
+//For REST Service
+import javax.ws.rs.*; 
+import javax.ws.rs.core.MediaType; 
+//For JSON
+import com.google.gson.*; 
+//For XML
+import org.jsoup.*; 
+import org.jsoup.parser.*; 
+import org.jsoup.nodes.Document; 
+@Path("/Employees") 
+public class EmployeeService 
+{ 
+ Employee employeeObj = new Employee(); 
 
-public class EmployeeService {
+
+@POST
+@Path("/") 
+@Consumes(MediaType.APPLICATION_FORM_URLENCODED) 
+@Produces(MediaType.TEXT_PLAIN) 
+public String insertEmployee(@FormParam("employeeid") String employeeid, 
+ @FormParam("employeename") String employeename, 
+ @FormParam("employeedob") String employeedob, 
+ @FormParam("employeeaddress") String employeeaddress,
+ @FormParam("employeegender") String employeegender, 
+ @FormParam("employeesalary") String employeesalary)
+{ 
+ String output = employeeObj.insertEmployee(employeename, employeedob, employeeaddress, employeegender, employeesalary); 
+return output; 
+}
+
+
+
+@GET
+@Path("/") 
+@Produces(MediaType.TEXT_HTML) 
+public String readItems() 
+ { 
+ return employeeObj.readEmployees(); 
+}
+
+
+
+@PUT
+@Path("/") 
+@Consumes(MediaType.APPLICATION_JSON) 
+@Produces(MediaType.TEXT_PLAIN) 
+public String updateEmployee(String employeeData) 
+{ 
+//Convert the input string to a JSON object 
+ JsonObject employeeObject = new JsonParser().parse(employeeData).getAsJsonObject(); 
+//Read the values from the JSON object
+ int employeeid = employeeObject.get("employeeid").getAsInt(); 
+ String employeename = employeeObject.get("employeename").getAsString(); 
+ String employeedob = employeeObject.get("employeedob").getAsString(); 
+ String employeeaddress = employeeObject.get("employeeaddress").getAsString(); 
+ String employeegender = employeeObject.get("employeegender").getAsString(); 
+ String employeesalary = employeeObject.get("employeesalary").getAsString();
+ String output = employeeObj.updateEmployee(employeeid, employeename, employeedob, employeeaddress, employeegender, employeesalary); 
+return output; 
+}
+
+
+
+@DELETE
+@Path("/") 
+@Consumes(MediaType.APPLICATION_XML) 
+@Produces(MediaType.TEXT_PLAIN) 
+public String deleteEmployee(String employeeData) 
+{ 
+//Convert the input string to an XML document
+ Document doc = Jsoup.parse(employeeData, "", Parser.xmlParser()); 
+ 
+//Read the value from the element <employeeid>
+ String employeeid = doc.select("employeeid").text(); 
+ String output = employeeObj.deleteEmployee(employeeid); 
+return output; 
+}
+
 
 }
+
--- src/model/Employee.java
+++ src/model/Employee.java
@@ -1,5 +1,173 @@
-package model;
+package model; 
+import java.sql.*; 
+public class Employee 
+{ //A common method to connect to the DB
+private Connection connect() 
+ { 
+ Connection con = null; 
+ try
+ { 
+ Class.forName("com.mysql.jdbc.Driver"); 
+ 
+ //Provide the correct details: DBServer/DBName, username, password 
+ con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/electrogrid", "root", ""); 
+ } 
+ catch (Exception e) 
+ {e.printStackTrace();} 
+ return con; 
+ }
+public String insertEmployee(String employeename, String employeedob, String employeeaddress, String employeegender, String employeesalary) 
+{ 
+String output = ""; 
+try
+{ 
+Connection con = connect(); 
+if (con == null) 
+{return "Error while connecting to the database for inserting."; } 
+// create a prepared statement
+String query = " insert into employee values (?, ?, ?, ?, ?,?)"; 
 
-public class Employee {
+PreparedStatement preparedStmt = con.prepareStatement(query); 
+// binding values
+preparedStmt.setInt(1, 0); 
+preparedStmt.setString(2, employeename); 
+preparedStmt.setString(3, employeedob); 
+preparedStmt.setString(4, employeeaddress); 
+preparedStmt.setString(5, employeegender);
+preparedStmt.setString(6, employeesalary);
 
+// execute the statement
+preparedStmt.execute(); 
+con.close(); 
+output = "Inserted employee successfully"; 
+} 
+catch (Exception e) 
+{ 
+output = "Error while inserting the employee."; 
+System.err.println(e.getMessage()); 
+} 
+return output; 
 }
+ 
+
+
+public String readEmployees() 
+{ 
+String output = ""; 
+try
+{ 
+Connection con = connect(); 
+if (con == null) 
+{return "Error while connecting to the database for reading."; } 
+// Prepare the html table to be displayed
+output = "<table border='1'><tr><th>Employee Name</th><th>Employee DOB</th>" +
+"<th>Employee Address</th>" + 
+"<th>Employee Gender</th>" +
+"<th>Employee Salary</th>" +
+"<th>Update</th><th>Remove</th></tr>"; 
+
+String query = "select * from employees"; 
+Statement stmt = con.createStatement(); 
+ResultSet rs = stmt.executeQuery(query); 
+// iterate through the rows in the result set
+while (rs.next()) 
+{ 
+int employeeid = rs.getInt("employeeid"); 
+String employeename = rs.getString("employeename"); 
+String employeedob = rs.getString("employeedob"); 
+String employeeaddress = rs.getString("employeeaddress"); 
+String employeegender = rs.getString("employeegender"); 
+String employeesalary = rs.getString("employeesalary"); 
+// Add into the html table
+    output += "<tr style=\"border: 1px solid #ddd; padding: 8px;\"><td style=\"padding-top: 6px; padding-bottom: 6px; text-align: center; color: Violet;\">" + employeeid + "</td>";
+	output += "<td style=\"padding-top: 6px; padding-bottom: 6px; text-align: center; color: #3B3B3B;\">" + employeename + "</td>";
+	output += "<td style=\"padding-top: 6px; padding-bottom: 6px; text-align: center; color: #3B3B3B;\">" + employeedob + "</td>";
+	output += "<td style=\"padding-top: 6px; padding-bottom: 6px; text-align: center; color: #3B3B3B;\">" + employeeaddress + "</td>";
+	output += "<td style=\"padding-top: 6px; padding-bottom: 6px; text-align: center; color: #3B3B3B;\">" + employeegender + "</td>";
+	output += "<td style=\"padding-top: 6px; padding-bottom: 6px; text-align: center; color: #3B3B3B;\">" + employeesalary + "</td>";
+
+} 
+con.close(); 
+// Complete the html table
+output += "</table>"; 
+} 
+catch (Exception e) 
+{ 
+output = "Error while reading the employees."; 
+System.err.println(e.getMessage()); 
+} 
+return output; 
+}
+
+
+
+public String updateEmployee(int employeeid, String employeename, String employeedob, String employeeaddress, String employeegender, String employeesalary)
+{		
+
+	 String output = "";
+	 try
+	 {
+	 Connection con = connect();
+	 if (con == null)
+	 {return "Error while connecting to the database for updating."; }
+	 // create a prepared statement
+	 String query = "UPDATE complaint SET employeename=?,employeedob=?,employeeaddress=?,employeegender=?,employeesalary=? WHERE complaintid=?";
+	 PreparedStatement preparedStmt = con.prepareStatement(query);
+	 // binding values
+	 
+	 preparedStmt.setString(1, employeename);
+	 preparedStmt.setString(2, employeedob);
+	 preparedStmt.setString(3, employeeaddress);
+	 preparedStmt.setString(4, employeegender);
+	 preparedStmt.setString(5, employeesalary);
+	 preparedStmt.setInt(6, employeeid);
+	 // execute the statement
+	 preparedStmt.execute();
+	 con.close();
+	 System.out.println(employeeid);
+	 return "Updated employee successfully";
+	 }
+	 
+	 catch (Exception e)
+	 {
+	 output = "Error while updating the employee.";
+	 System.err.println(e.getMessage());
+	 }
+	 return output;
+	 }
+
+
+
+public String deleteEmployee(String employeeid) {
+	
+	 String output = "";
+	 try
+	 {
+	 Connection con = connect();
+	 if (con == null)
+	 {return "Error while connecting to the database for deleting."; }
+	 // create a prepared statement
+	 String query = "delete from complaint where employeeid=?";
+	 PreparedStatement preparedStmt = con.prepareStatement(query);
+	 // binding values
+	 preparedStmt.setInt(1, Integer.parseInt(employeeid));
+	 // execute the statement
+	 preparedStmt.execute();
+	 con.close();
+	 output = "Deleted employee successfully";
+	 }
+	 catch (Exception e)
+	 {
+	 output = "Error while deleting the employee";
+	 System.err.println(e.getMessage());
+	 }
+	 return output;
+	 }
+	}
+	
+
+
+
+
+
+
